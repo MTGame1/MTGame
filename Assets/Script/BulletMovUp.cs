f298cf452c0bd554dba75f128b034ee6a1bcb069
ericqin <ericqin@tencent.com>
2016-05-30 20:36:04 +0800

    小改动

---
+++ Assets/Script/BulletMovUp.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Assets.Script
+{
+public class BulletMovUp : MonoBehaviour {
+	private bool istrigger = false;
+	private GameObject bullect;
+	private GameObject myparent;
+		private GameObject bullet1;
+
+		private Bullet b_bullet1;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+		string path3 = "Prefab/zidan";
+		this.bullect = Resources.Load(path3) as GameObject;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if(bullet1){
+				if (b_bullet1.right) {
+					bullet1.transform.localPosition = new Vector3 (bullet1.transform.localPosition.x + 600f * Time.deltaTime, bullet1.transform.localPosition.y+ 300f * Time.deltaTime, bullet1.transform.localPosition.z);
+				} else {
+					bullet1.transform.localPosition = new Vector3 (bullet1.transform.localPosition.x - 600f * Time.deltaTime, bullet1.transform.localPosition.y+ 300f * Time.deltaTime, bullet1.transform.localPosition.z);
+
+				}
+			}
+
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.name == "bu") {
+			Destroy (other.gameObject);
+
+			istrigger = true;
+
+				BattleScene._bulletNum--;
+
+			GameObject otherObject = other.gameObject;
+			Bullet otherBuller=otherObject.GetComponent<Bullet>();
+		
+
+			string path = "Prefab/bullet";
+			GameObject obj = Resources.Load(path) as GameObject;
+			bullet1 = GameObject.Instantiate(obj);
+			bullet1.transform.SetParent(transform.parent.transform, false);
+			bullet1.transform.position = otherObject.transform.position;
+
+			b_bullet1 = bullet1.AddComponent<Bullet>();
+			b_bullet1.right = otherBuller.right;
+			BattleScene._bulletNum++;
+
+	
+
+
+        }
+	}
+
+}
+}
---
+++ Assets/Script/BulletMoveBack.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Assets.Script
+{
+public class BulletMoveBack : MonoBehaviour {
+	private bool istrigger = false;
+	private GameObject bullect;
+	private GameObject myparent;
+
+		private GameObject bullet3;
+	
+		private Bullet b_bullet3;
+
+	// Use this for initialization
+	void Start () {
+		
+		string path3 = "Prefab/zidan";
+		this.bullect = Resources.Load(path3) as GameObject;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+			if(bullet3){
+				if (b_bullet3.right) {
+					bullet3.transform.localPosition = new Vector3 (bullet3.transform.localPosition.x - 600f * Time.deltaTime, bullet3.transform.localPosition.y, bullet3.transform.localPosition.z);
+				} else {
+					bullet3.transform.localPosition = new Vector3 (bullet3.transform.localPosition.x + 600f * Time.deltaTime, bullet3.transform.localPosition.y, bullet3.transform.localPosition.z);
+
+				}
+			}
+
+
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.name == "bu") {
+			Destroy (other.gameObject);
+
+			istrigger = true;
+
+			BattleScene._bulletNum--;
+
+			GameObject otherObject = other.gameObject;
+			Bullet otherBuller=otherObject.GetComponent<Bullet>();
+		
+
+			string path = "Prefab/bullet";
+			GameObject obj = Resources.Load(path) as GameObject;
+		
+
+
+			bullet3 = GameObject.Instantiate(obj);
+			bullet3.transform.SetParent(transform.parent.transform, false);
+			bullet3.transform.position = otherObject.transform.position;
+			b_bullet3 = bullet3.AddComponent<Bullet>();
+			b_bullet3.right = otherBuller.right;
+			BattleScene._bulletNum++;
+
+
+        }
+	}
+
+}
+}
